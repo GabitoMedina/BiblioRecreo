cfc40353b3bebd05b503a7c766c06b3e16186085
Edison <santy_g33@hotmail.com>
2018-04-24 12:46:39 +0200

    Nuevo terminado detalle ingreso

--- cgvVentas/resources/views/proformas/modal.blade.php
+++
@@ -1,6 +0,0 @@
-@extends ('layouts.admin')
-@section ('contenido')
-	<div>
-		<h1>modal proform</h1>
-	</div>
-@endsection
---
+++ cgvVentas1/app/Ingreso.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace cgvVentas;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingreso extends Model
+{
+    protected $table='ingreso';
+    protected $primaryKey = 'idingreso';
+
+    public $timestamps=false;
+
+    protected $fillable = [
+    	'idproveedor',
+    	'tipo_comprobante',
+    	'num_comrobante',
+    	'fecha',
+    	'iva',
+    	'estado'
+    ];
+    protected $guarded =[
+        ];
+
+}
